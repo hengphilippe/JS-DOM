f2f5be51f0b2fcff69ae2711e84d5cef36cd0d8e
lynchzival <soksovandavid.gmail.com>
2021-06-05 15:41:08 +0700

    prevent duplicated value from adding to the list

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
-// get paraent List
-const allTasks = document.querySelector('#book-list ul');
 
-// delete list 
-
+	const allTasks = document.querySelector('#book-list ul');
 	const btnDel = document.querySelectorAll(".delete");
-	// for(let i=0; i<btnDel.length;i++){
-	// 	console.log(btnDel[i]);
-	// 
+	const addForm = document.forms['add-list'];
+	let newTaskDuplicated = false;
+
+	addForm.addEventListener('submit',function(e) {
+		e.preventDefault();
+
+		const formInput = addForm.querySelector('input[type="text"]');
+		const newTask = formInput.value.trim();
+		let taskName = allTasks.querySelectorAll('li > span.name');
+
+		taskName.forEach(i => {
+			const allTasksValue = i.textContent.toLowerCase();
+			const newTaskInput = newTask.toLowerCase();
+
+			//set to true if duplicated found
+			if (allTasksValue == newTaskInput) {
+				newTaskDuplicated = true;
+				alert("Duplicated Value Can't be Stored.");
+			}
+		});
+
+		if (!(newTaskDuplicated)) {
+			const taskRow = document.createElement('li');
+			const newTaskName = document.createElement('span');
+			const deleteTask = document.createElement('span');
+
+			// set value
+			newTaskName.textContent=newTask;
+			deleteTask.textContent='delete';
+
+			// set style 
+			newTaskName.classList.add('name');
+			deleteTask.classList.add('delete');
+
+			// append chile node
+			taskRow.appendChild(newTaskName);
+			taskRow.appendChild(deleteTask);
+
+			allTasks.appendChild(taskRow);
+		}
+
+		// clear input value
+		formInput.value = "";
+
+		// reset back to false
+		newTaskDuplicated = false;
+
+	});
 
 	for(let btn of btnDel) {
 		btn.addEventListener('click', (e) => {
@@ -16,36 +58,3 @@ const allTasks = document.querySelector('#book-list ul');
 			allRec.removeChild(rec);
 		})
 	};
-
-// Add new task (FORM)
-
-	const addForm = document.forms['add-list'];
-	addForm.addEventListener('submit',function(e) {
-		// console.log("doing click");
-		e.preventDefault();
-		const formInput = addForm.querySelector('input[type="text"]');
-		const newTask = formInput.value;
-		
-		// create elements (li span span)
-		const taskRow = document.createElement('li');
-
-		const newTaskName = document.createElement('span');
-		const deleteTask = document.createElement('span');
-
-		// set value
-		newTaskName.textContent=newTask;
-		deleteTask.textContent='delete';
-
-		// set style 
-		newTaskName.classList.add('name');
-		deleteTask.classList.add('delete');
-
-		// append chile node
-		taskRow.appendChild(newTaskName);
-		taskRow.appendChild(deleteTask);
-
-		allTasks.appendChild(taskRow);
-
-		formInput.value = "";
-
-	});
