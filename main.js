54cdb60032dba3daa622893651a4a7a418ee7b1c
Samnang Nhum <samnanggikboy@gmail.com>
2021-06-27 22:35:17 +0700

    New Function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // get paraent List
 const allTasks = document.querySelector('#book-list ul');
 
+
 // delete list 
 function deleleList(){
 	const btnDel = document.querySelectorAll(".delete");
@@ -16,6 +17,7 @@ function deleleList(){
 			let allRec = rec.parentNode;
 			
 			allRec.removeChild(rec);
+			
 		})
 	};
 	}
@@ -44,7 +46,7 @@ function deleleList(){
           	// create elements (li span span)
 		const taskRow = document.createElement('li');
 	
-		taskRow.innerHTML="<span class='name'>"+ newTask +"</span><span class='delete'>delete</span>";
+		taskRow.innerHTML="<input type='checkbox' />&nbsp<span class='name'>"+ newTask +"</span><span class='delete'>delete</span>";
 
 		// const newTaskName = document.createElement('span');
 		// const deleteTask = document.createElement('span');
@@ -62,9 +64,10 @@ function deleleList(){
 		// taskRow.appendChild(deleteTask);
 
 				allTasks.appendChild(taskRow);
-			
+				
 			// Delete Function Fire again for query new element
 				deleleList()
+				checkAllTask()
 		}
 			
 		
@@ -76,3 +79,89 @@ function deleleList(){
 		
 	
 	});
+
+	// Search
+	const searchForm = document.forms['search-books']
+	const inputSearch = searchForm.querySelector('input[type="text"]')
+
+	inputSearch.addEventListener('keyup' , (e) => {
+		const searchVal = e.target.value.toLowerCase()
+		const taskNames = allTasks.querySelectorAll('.name') 
+
+		for(let i = 0 ; i < taskNames.length; i++){
+			if(taskNames[i].textContent.toLowerCase().indexOf(searchVal) == -1 ){
+				taskNames[i].parentNode.style.display = 'none'
+			} else {
+				taskNames[i].parentNode.style.display = 'block'
+			}
+		}
+	})
+
+	///// checked Hidden list
+
+	const checkHidden = document.forms['add-list'].querySelector('input[type="checkbox"]')
+
+	checkHidden.addEventListener('change' , (e)=>{
+		let checkedBox = e.target.checked
+
+		allTasks.style.display = checkedBox ? 'none' : 'block' ;
+
+	})
+
+
+	// Checked Complete list &  expand
+	function checkAllTask(){
+
+		const checkAllTask = allTasks.querySelectorAll('input[type="checkbox"]')
+		let completeTasks = document.querySelector('#complete-tasks ul')
+		
+		
+		// check completeList
+		for(let checkAll of checkAllTask){
+		
+			checkAll.addEventListener('change' , (e)=> {
+				let checkedBox = e.target.checked
+			
+				if(checkedBox){
+					completeTasks.appendChild(checkAll.parentNode)
+					let delBtn = completeTasks.querySelector('.delete')
+					delBtn.style.display = "none"
+
+				} else {
+					
+					let delBtn = completeTasks.querySelector('.delete')
+					delBtn.style.display = "block"
+					allTasks.appendChild(checkAll.parentNode)
+				}
+						
+				let completeTasksBadge = document.querySelector('#complete-tasks .badge')
+				let completeTasklength = document.querySelectorAll('#complete-tasks ul li')
+	
+				completeTasksBadge.innerHTML = completeTasklength.length
+			})
+		}
+		/// Expand
+		const arrow = document.querySelector('#complete-title .material-icons')
+		arrow.addEventListener('click' , (e)=> {
+			if(e.target.textContent === "expand_more"){
+				e.target.textContent = "expand_less"
+			} else {
+				e.target.textContent = "expand_more"
+			}
+
+			if(completeTasks.classList.contains('hidden')){
+				completeTasks.classList.remove('hidden')
+			} else {
+				completeTasks.classList.add('hidden')
+			}
+		})
+
+		////////
+	
+	}
+
+	checkAllTask()
+
+	///
+
+	
